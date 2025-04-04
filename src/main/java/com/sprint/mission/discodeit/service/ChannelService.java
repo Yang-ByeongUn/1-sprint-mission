b8b4cd6f96f206172eeaef48ce5ec2d47a071523
Yang-ByeongUn <ypd06426@naver.com>
2025-04-05 00:41:57 +0900

    feat: fix codes after merge

--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -1,6 +1,5 @@
 package com.sprint.mission.discodeit.service;
 
-<<<<<<< HEAD
 import com.sprint.mission.discodeit.dto.data.ChannelDto;
 import com.sprint.mission.discodeit.dto.request.PrivateChannelCreateRequest;
 import com.sprint.mission.discodeit.dto.request.PublicChannelCreateRequest;
@@ -22,23 +21,3 @@ public interface ChannelService {
 
   void delete(UUID channelId);
 }
-=======
-import com.sprint.mission.discodeit.entity.Channel;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
-
-public interface ChannelService {
-    Channel createChannel(String channelName);
-
-    Map<UUID, Channel> getChannels();
-
-    Optional<Channel> getChannel(UUID uuid);
-
-    Optional<Channel> addMessageToChannel(UUID channelUUID, UUID messageUUID);
-
-    Optional<Channel> updateChannel(UUID uuid, String channelName);
-
-    Optional<Channel> deleteChannel(UUID uuid);
-}
->>>>>>> 67d19b8276e693bf808f9d9f2844e6ad4539f06b
--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,20 +1,15 @@
 package com.sprint.mission.discodeit.service;
 
-<<<<<<< HEAD
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
 import java.util.List;
-=======
-import com.sprint.mission.discodeit.entity.User;
-import java.util.Map;
->>>>>>> 67d19b8276e693bf808f9d9f2844e6ad4539f06b
+
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserService {
-<<<<<<< HEAD
 
   UserDto create(UserCreateRequest userCreateRequest,
       Optional<BinaryContentCreateRequest> profileCreateRequest);
@@ -27,15 +22,5 @@ public interface UserService {
       Optional<BinaryContentCreateRequest> profileCreateRequest);
 
   void delete(UUID userId);
-=======
-    User createUser(String username);
 
-    Map<UUID, User> getUsers();
-
-    Optional<User> getUser(UUID uuid);
-
-    Optional<User> updateUser(UUID uuid, String username);
-
-    Optional<User> deleteUser(UUID uuid);
->>>>>>> 67d19b8276e693bf808f9d9f2844e6ad4539f06b
 }
